450eaf4de704cc7b1b10b35d223b685a79f41767
Manzi453 <i.manziyamu@alustudent.com>
2026-03-23 11:12:06 +0200

    correction of some errors within the providers folder

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/auth_provider_provider.dart';
+import '../../providers/auth_provider.dart';
 import '../../theme/app_theme.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
